cf2b8be3a3ffcff744023bc963c196deb1d97946
chasereinders-dot <chase.reinders@gmail.com>
2026-01-31 10:49:05 -0600

    Set the correct indexer states and add passing state.

--- src/main/java/frc/robot/operator/OperatorIntentEvents.java
+++ src/main/java/frc/robot/operator/OperatorIntentEvents.java
@@ -22,4 +22,6 @@ public interface OperatorIntentEvents {
   Trigger wantsToOuttake();
 
   Trigger wantsToShoot();
+
+  Trigger wantsToPass();
 }
--- src/main/java/frc/robot/subsystems/indexer/IndexerBehavior.java
+++ src/main/java/frc/robot/subsystems/indexer/IndexerBehavior.java
@@ -13,6 +13,16 @@ public class IndexerBehavior extends SubsystemBehavior {
   @Override
   public void configure(AllEvents events) {
     events.goals().isIdleTrigger().whileTrue(indexer.idleCommand());
-    events.goals().isShootingTrigger().whileTrue(indexer.indexingCommand());
+    events
+        .goals()
+        .isShootingTrigger()
+        .or(events.goals().isPassingTrigger())
+        .whileTrue(indexer.indexingCommand());
+    events.goals().isIntakingTrigger().whileTrue(indexer.indexingCommand());
+    events.goals().isOuttakingTrigger().whileTrue(indexer.idleCommand());
+    events.goals().isClimbingL0().whileTrue(indexer.idleCommand());
+    events.goals().isClimbingL1().whileTrue(indexer.idleCommand());
+    events.goals().isClimbingL2().whileTrue(indexer.idleCommand());
+    events.goals().isClimbingL3().whileTrue(indexer.idleCommand());
   }
 }
