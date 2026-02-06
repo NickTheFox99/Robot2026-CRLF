bd02ba94c2c480d9f3f8762ddd1236c5b9b61d1d
avmhh <hutchisonava14@gmail.com>
2026-02-05 18:08:51 -0600

    fixed crash resulting from turretIOTalonFX.java

--- src/main/java/frc/robot/subsystems/shooter/ShooterIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIOTalonFX.java
@@ -14,8 +14,6 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.MotorAlignmentValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
-
-import edu.wpi.first.units.AngularVelocityUnit;
 import edu.wpi.first.units.measure.AngularVelocity;
 import frc.robot.util.Gains;
 import frc.robot.util.PhoenixUtil;
