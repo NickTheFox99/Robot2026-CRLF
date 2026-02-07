dda36d2c543a38ee274a664956341c4c0d992136
EphraimSonnenberg <ephraimsonnenberg@gmail.com>
2026-02-07 13:51:48 -0600

    Added PhotonVisionSim

--- src/main/java/frc/robot/subsystems/vision/VisionIOPhotonVisionSim.java
+++ src/main/java/frc/robot/subsystems/vision/VisionIOPhotonVisionSim.java
@@ -1,16 +1,24 @@
-// Copyright (c) 2021-2026 Littleton Robotics
+// Copyright 2021-2025 FRC 6328
 // http://github.com/Mechanical-Advantage
 //
-// Use of this source code is governed by a BSD
-// license that can be found in the LICENSE file
-// at the root directory of this project.
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// version 3 as published by the Free Software Foundation or
+// available in the root directory of this project.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
 
 package frc.robot.subsystems.vision;
 
 import static frc.robot.subsystems.vision.VisionConstants.aprilTagLayout;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import java.util.function.Supplier;
 import org.photonvision.simulation.PhotonCameraSim;
 import org.photonvision.simulation.SimCameraProperties;
@@ -38,15 +46,34 @@ public class VisionIOPhotonVisionSim extends VisionIOPhotonVision {
     if (visionSim == null) {
       visionSim = new VisionSystemSim("main");
       visionSim.addAprilTags(aprilTagLayout);
+      // https://docs.photonvision.org/en/v2025.1.1/docs/simulation/simulation-java.html
+      SmartDashboard.putData("cameraField", visionSim.getDebugField());
     }
 
     // Add sim camera
     var cameraProperties = new SimCameraProperties();
-    cameraSim = new PhotonCameraSim(camera, cameraProperties, aprilTagLayout);
+    // A 640 x 480 camera with a 100 degree diagonal FOV.
+    cameraProperties.setCalibration(640, 480, Rotation2d.fromDegrees(80));
+    // Approximate detection noise with average and standard deviation error in pixels.
+    cameraProperties.setCalibError(0.25, 0.08);
+    // Set the camera image capture framerate (Note: this is limited by robot loop rate).
+    cameraProperties.setFPS(120);
+    // The average and standard deviation in milliseconds of image data latency.
+    cameraProperties.setAvgLatencyMs(35);
+    cameraProperties.setLatencyStdDevMs(5);
+    cameraSim = new PhotonCameraSim(camera, cameraProperties);
+    // Enable the raw and processed streams. These are enabled by default.
+    cameraSim.enableRawStream(true);
+    cameraSim.enableProcessedStream(true);
+
+    // Enable drawing a wireframe visualization of the field to the camera streams.
+    // This is extremely resource-intensive and is disabled by default.
+    cameraSim.enableDrawWireframe(true);
     visionSim.addCamera(cameraSim, robotToCamera);
   }
 
   @Override
+  // updates the pose supplier and implements updateInputs
   public void updateInputs(VisionIOInputs inputs) {
     visionSim.update(poseSupplier.get());
     super.updateInputs(inputs);
